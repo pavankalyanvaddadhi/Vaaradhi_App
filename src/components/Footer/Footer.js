53f1bbdc33e49a5788b795ce06a6f913e6c67a2d
pavankalyanvaddadhi <pavankalyan111101@gmail.com>
2024-12-14 20:27:49 +0530

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,94 @@
-import React from "react";
-const Footer=()=>{
-    return(
-        <>
-        <h1>hello !!!!!!!</h1>
-        </>
-    )
+import React from 'react'
+import { FaInstagram } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa6";
+const Footer = () => {
+  const tabledata = [
+    {
+      name1: 'FAQ',
+      name2: ' Trade Name : Vaaradhi Farms',
+    },
+    {
+      name1: 'Shipping Policy',
+      name2: 'Email Support : vaaradhifarms@gmail.com',
+    },
+    {
+      name1: 'Refund Policy',
+      name2: 'Shipping Policy',
+    },
+    {
+      name1: 'Refund Polocy',
+      name2: 'Customer Care Number :',
+    },
+    {
+      name1: 'Cancellation Policy',
+      name2: '+91 89770 21193',
+    },
+    {
+      name1: 'Privacy Policy',
+      name2: '+91 89770 21197',
+    },
+    {
+      name1: 'Terms of Service',
+      name2: 'Customer Support Timings : 10:30am to 6:30pm',
+    },
+    {
+      name1: 'Contact Information',
+      name2: 'Address : Manikonda, Hyderabad, Telangana , Pin - 500089',
+    },
+    {
+      name1: 'Wild forest honey certificate',
+      name2: 'FSSAI : 23623028001252',
+    },
+  ]
+
+  return (
+    <>
+      <table class="container-fluid" style={{backgroundColor:"#F8C027"}}>
+        <tr >
+          <th style={{paddingLeft:"50px",paddingTop:"50px"}}>Customer Care</th>
+
+          <th style={{paddingTop:"50px"}}>Contact info</th>
+        </tr>
+
+        {tabledata.map(copyoftabledata => {
+          const {
+            name1,
+            name2,
+            name3,
+            name4,
+            name5,
+            name6,
+            name7,
+            name8,
+            name9,
+          } = copyoftabledata
+          return (
+            <>
+              <tr>
+                <td style={{paddingLeft:"50px",paddingTop:"20px"}}>{name1}</td>
+
+                <td  style={{paddingRight:"300px"}}>{name2}</td>
+              </tr>
+            </>
+          )
+        })}
+     <div className='container' style={{padding:"30px"}}>
+     <FaInstagram />
+     </div>
+      <div className='container' style={{padding:"30px",display:"flex"}}>
+        <div>
+        <FaRegCopyright />
+        </div>
+        <div style={{paddingLeft:"10px"}}>
+            <p>
+            2024, Vaaradhi Farms. Built by Ecommified.in
+            </p>
+        </div>
+     
+      </div>
+      
+      </table>
+    </>
+  )
 }
 export default Footer
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,23 +4,44 @@ import {CiSearch} from 'react-icons/ci' //Search icon
 import {IoPerson} from 'react-icons/io5' //Account Icon
 import {IoCartOutline} from 'react-icons/io5' //Cart Icon
 const Navbar = ({updatesearchdata}) => {
-  const handlesearch =(event)=>{
+  const handlesearch = event => {
     updatesearchdata(event.target.value)
   }
   return (
     <>
-<div className='row'style={{backgroundColor:"#F8C027", padding:30}}>
-<div className="col-2" style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+    <div className='row' style={{backgroundColor:"green",color:"white",fontWeight:"1000"}}>
+      <h4 style={{display:"flex",justifyContent:"center"}} >
+      Free Shipping On Orders Above ₹999
+      </h4>
+    </div>
+      <div className="row" style={{backgroundColor: '#F8C027', padding: 30}}>
+        <div
+          className="col-2"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
           <FaAlignJustify />
         </div>
-        <div className="col-6" style={{display:'flex', justifyContent:'center'}}>
-          <img src="https://vaaradhifarms.com/cdn/shop/files/Asset_1_2x_399eef15-89ca-42cd-ac92-8f75b5e60552.png?v=1685440038&width=370" style={{width:185,height:107}}/>
+        <div
+          className="col-6"
+          style={{display: 'flex', justifyContent: 'center'}}>
+          <img
+            src="https://vaaradhifarms.com/cdn/shop/files/Asset_1_2x_399eef15-89ca-42cd-ac92-8f75b5e60552.png?v=1685440038&width=370"
+            style={{width: 185, height: 107}}
+          />
         </div>
-        <div className="col-4"style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
-          <div className="row" >
+        <div
+          className="col-4"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <div className="row">
             <div className="col">
-            
-            <input onChange={handlesearch} type="text" placeholder="Search" />
+              <input onChange={handlesearch} type="text" placeholder="Search" />
             </div>
             <div className="col">
               <IoPerson />
@@ -30,8 +51,7 @@ const Navbar = ({updatesearchdata}) => {
             </div>
           </div>
         </div>
-</div>
-       
+      </div>
     </>
   )
 }
